7d644381d5f26e5ee64cf057347378663c432ce1
yoshimitsu198 <yoshimitsu198@users.noreply.github.com>
2025-11-13 03:57:31 +0200

    perf: add error messages

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+// Updated iteration 2
+function func2(): boolean {
+    return true;
+}
+
+function processData2(data: string): string | null {
+    if (data) {
+        return data.toUpperCase();
+    }
+    return null;
+}
